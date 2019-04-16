531a24ffc8f9fc13f7df97484a4d78bd4bfe51d9
unknown <wangzhiguo110>
2019-04-16 18:32:31 +0800

    hongbao

---
+++ app/Model/Bonus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bonus extends Model
+{
+    //红包表
+    protected $table="jy_bonus";
+    public $timestamps = true;
+}
